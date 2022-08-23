b6feec2c3833ecdb3b898a4af3f685ce00e88fa4
Matoskipo <matthew.ataekong@decagon.dev>
2022-08-23 12:06:32 +0100

    Git users app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Setup from './setup/git'
+import './index.css';
+
+function App() {
+  return(
+    <div className='container'>
+      <Setup />
+    </div>
+  )
+}
+
+export default App;
